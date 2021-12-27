62f6981318ee0ad110c88d1c0b25f4e7769d9b19
Erick Estrada <erickestradapj@gmail.com>
2021-12-27 17:02:56 -0500

    graphic added

--- src/app/pages/graph1/graph1.component.ts
+++ src/app/pages/graph1/graph1.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType, Color } from 'chart.js';
 
 @Component({
   selector: 'app-graph1',
   templateUrl: './graph1.component.html',
-  styleUrls: ['./graph1.component.css']
+  styleUrls: ['./graph1.component.css'],
 })
 export class Graph1Component implements OnInit {
+  // Doughnut
+  public doughnutChartLabels: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
 
-  constructor() { }
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      {
+        data: [350, 450, 100],
+        backgroundColor: ['#6857E6', '#009FEE', '#F02059'],
+      },
+    ],
+  };
 
-  ngOnInit(): void {
-  }
+  public doughnutChartType: ChartType = 'doughnut';
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
+import { SharedModule } from '../shared/shared.module';
+import { NgChartsModule } from 'ng2-charts';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graph1Component } from './graph1/graph1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
@@ -29,6 +31,7 @@ import { ComponentsModule } from '../components/components.module';
     RouterModule,
     FormsModule,
     ComponentsModule,
+    NgChartsModule,
   ],
 })
 export class PagesModule {}
