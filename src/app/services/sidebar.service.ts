4d19eaa0c4642eebaaeb17897d4eb9c8c8beecd7
Erick Estrada <erickestradapj@gmail.com>
2021-12-27 23:30:03 -0500

    sidebar service added

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SidebarService {
+  public menu: any[] = [
+    {
+      title: 'Dashboard',
+      icon: 'mdi mdi-gauge',
+      submenu: [
+        { title: 'Main', url: '/' },
+        { title: 'ProgressBar', url: 'progress' },
+        { title: 'Graphics', url: 'graph1' },
+      ],
+    },
+  ];
+
+  constructor() {}
+}
