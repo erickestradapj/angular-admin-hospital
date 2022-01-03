c7ecfb137ba83c7cefd5b7bf97c8fd378affa16b
Erick Estrada <erickestradapj@gmail.com>
2022-01-02 20:02:22 -0500

    valid form added

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
   exports: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
 })
 export class AuthModule {}
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  constructor() {}
+  public formSubmitted = false;
+
+  public registerForm = this.fb.group({
+    name: ['Erick Estrada', Validators.required],
+    email: ['test1@test.com', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    password2: ['', Validators.required],
+    terms: [false, Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  public createUser() {
+    this.formSubmitted = true;
+    console.log(this.registerForm);
+    console.log(this.registerForm.value);
+    // console.log(this.registerForm.get('name')?.invalid);
+
+    if (this.registerForm.valid) {
+      console.log('Send form...');
+    } else {
+      console.log('Form incorrect');
+    }
+  }
+
+  public fieldNotValid(field: string): boolean {
+    if (this.registerForm.get(field)?.invalid && this.formSubmitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public acceptTerms() {
+    return !this.registerForm.get('terms')?.value && this.formSubmitted;
+  }
 }
