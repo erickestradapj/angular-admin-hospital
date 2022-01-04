c4f1465c434b8ef3ab315f5d37b4d3c1dee819a9
Erick Estrada <erickestradapj@gmail.com>
2022-01-04 01:22:12 -0500

    google sign-in added

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import Swal from 'sweetalert2';
+
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -7,11 +12,81 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router) {}
+  public auth2: any;
 
-  ngOnInit(): void {}
+  public loginForm = this.fb.group({
+    email: [
+      localStorage.getItem('email') || '',
+      [Validators.required, Validators.email],
+    ],
+    password: ['', Validators.required],
+    remember: [false],
+  });
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.renderButton();
+  }
 
   login() {
-    this.router.navigateByUrl('/');
+    this.userService.login(this.loginForm.value).subscribe(
+      (resp) => {
+        if (this.loginForm.get('remember')?.value) {
+          localStorage.setItem('email', this.loginForm.get('email')?.value);
+        } else {
+          localStorage.removeItem('email');
+        }
+      },
+      (e) => {
+        Swal.fire('Error', e.error.msg, 'error');
+      }
+    );
+    // console.log(this.loginForm.value);
+    // this.router.navigateByUrl('/');
+  }
+
+  renderButton() {
+    gapi.signin2.render('my-signin2', {
+      scope: 'profile email',
+      width: 240,
+      height: 50,
+      longtitle: true,
+      theme: 'dark',
+    });
+
+    this.startApp();
+  }
+
+  startApp() {
+    gapi.load('auth2', () => {
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      this.auth2 = gapi.auth2.init({
+        client_id:
+          '464448232482-i832666japgaklpc2jkvlulogbpad6cu.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+      });
+      this.attachSignin(document.getElementById('my-signin2'));
+    });
+  }
+
+  attachSignin(element: any) {
+    this.auth2.attachClickHandler(
+      element,
+      {},
+      (googleUser: any) => {
+        const id_token = googleUser.getAuthResponse().id_token;
+        this.userService.loginGoogle(id_token).subscribe();
+
+        // TODO: Redirect to dashboard
+      },
+      (error: any) => {
+        alert(JSON.stringify(error, undefined, 2));
+      }
+    );
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { RegisterForm } from '../interfaces/register-form.interface';
+import { LoginForm } from '../interfaces/login-form.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +14,20 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   createUser(formData: RegisterForm): Observable<RegisterForm> {
-    return this.http.post<RegisterForm>(`${this.baseUrl}/users`, formData);
+    return this.http
+      .post<RegisterForm>(`${this.baseUrl}/users`, formData)
+      .pipe(tap((resp: any) => localStorage.setItem('token', resp.token)));
+  }
+
+  login(formData: LoginForm): Observable<LoginForm> {
+    return this.http
+      .post<LoginForm>(`${this.baseUrl}/login`, formData)
+      .pipe(tap((resp: any) => localStorage.setItem('token', resp.token)));
+  }
+
+  loginGoogle(token: any) {
+    return this.http
+      .post(`${this.baseUrl}/login/google`, { token })
+      .pipe(tap((resp: any) => localStorage.setItem('token', resp.token)));
   }
 }
